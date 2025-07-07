968f878ee0f39e6adece71dbfc3f1f5bc8acce85
Miuris.G <miuris.gutierrez@gmail.com>
2025-07-06 20:34:02 -0400

    Updated js file, added variables and first function

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const inputTask = document.getElementById("inputTask");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskOutput = document.getElementById("taskOutput");
 
+function addTask() {
+  const taskText = inputTask.value;
+
+  const li = document.createElement("li");
+  li.innerHTML = `$("taskOutput") <button class="delete">Delete</button>`
+  taskOutput.appendChild(li);
+  inputTask.value = "";  
+}
+
+addTaskBtn.addEventListener("click", addTask);
